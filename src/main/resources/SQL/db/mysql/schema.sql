14fbea8601d1563009bf8127e487fa5f820fa45d
kavou <kavourdiris@windowslive.com>
2019-02-05 13:25:42 +0200

    -added/updated application.properties files -updated h2,mysql sql scripts

--- src/main/resources/SQL/db/mysql/schema.sql
+++ src/main/resources/SQL/db/mysql/schema.sql
@@ -1,10 +1,6 @@
---OneToMany (one game -> many bets)
-
 DROP SCHEMA IF EXISTS `betting_crawler`;
-
 CREATE SCHEMA `betting_crawler`;
-
-use `betting_crawler`;
+USE `betting_crawler`;
 
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -21,7 +17,7 @@ CREATE TABLE `game` (
     `time` time DEFAULT NULL,
     `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1;
 
 DROP TABLE IF EXISTS `bet`;
 
@@ -35,7 +31,7 @@ CREATE TABLE `bet` (
     `game_id` int(11) DEFAULT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1;
 
 DROP TABLE IF EXISTS `football_bet`;
 
@@ -43,20 +39,16 @@ CREATE TABLE `football_bet` (
     `bet_id` int(11) NOT NULL AUTO_INCREMENT,
     `draw` decimal(5,2) DEFAULT NULL,
     FOREIGN KEY (`bet_id`) REFERENCES `bet` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1;
 
 SET FOREIGN_KEY_CHECKS = 1;
-
---Database accept greek characters
 
 ALTER DATABASE `betting_crawler` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE `game` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE `bet` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE `football_bet` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
---Added extra fields to football_bet table
-
-ALTER TABLE football_bet
+ALTER TABLE `football_bet`
 ADD `over_0,5` decimal(5,2) DEFAULT NULL,
 ADD `over_1,5` decimal(5,2) DEFAULT NULL,
 ADD `over_2,5` decimal(5,2) DEFAULT NULL,
@@ -84,5 +76,4 @@ ADD `X/X` decimal(5,2) DEFAULT NULL,
 ADD `2/X` decimal(5,2) DEFAULT NULL,
 ADD `1/2` decimal(5,2) DEFAULT NULL,
 ADD `X/2` decimal(5,2) DEFAULT NULL,
-ADD `2/2` decimal(5,2) DEFAULT NULL
-;
+ADD `2/2` decimal(5,2) DEFAULT NULL;
