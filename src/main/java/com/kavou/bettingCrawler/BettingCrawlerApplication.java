de36e5a14e0d7170066cefce5a8289e4f6399390
kavou <kavourdiris@windowslive.com>
2019-02-06 16:32:11 +0200

    Removed ( temporarily) the main method from BettingCrawlerApplication

--- src/main/java/com/kavou/bettingCrawler/BettingCrawlerApplication.java
+++ src/main/java/com/kavou/bettingCrawler/BettingCrawlerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BettingCrawlerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BettingCrawlerApplication.class, args);
-	}
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(BettingCrawlerApplication.class, args);
+	// }
 
 }
 
