2b7f2707f340f87c367b4eed8bc5fea895080975
kavou <kavourdiris@windowslive.com>
2019-02-18 17:09:04 +0200

    User can now choose betwwen server and crawling mode

--- src/main/java/com/kavou/bettingCrawler/crawler/application/StoiximanApp.java
+++ src/main/java/com/kavou/bettingCrawler/crawler/application/StoiximanApp.java
@@ -1,13 +1,16 @@
-package com.kavou.bettingCrawler.crawler.applications;
+package com.kavou.bettingCrawler.crawler.application;
 
-import com.kavou.bettingCrawler.crawler.bettors.Stoiximan;
+import com.kavou.bettingCrawler.crawler.bettor.StoiximanParser;
+
 import me.tongfei.progressbar.ProgressBar;
 import org.jsoup.nodes.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,12 +33,15 @@ public class StoiximanApp {
 
     public static void main(String[] args) {
 
+        // program started
+        double startTime = System.nanoTime();
+
         ApplicationContext context = SpringApplication.run(StoiximanApp.class, args);
-        // get the bean for Stoiximan
-        Stoiximan stoiximan = context.getBean(Stoiximan.class);
+        // get the bean for StoiximanParser
+        StoiximanParser stoiximanParser = context.getBean(StoiximanParser.class);
 
         // give user the choice to crawl for data
-        System.out.println(ANSI_RED+"\nPress 1 to crawl for data. Application now is running in server mode!"+ANSI_RESET);
+        System.out.println(ANSI_RED+"\nApplication is now running in server mode ... Press 1 to start extracting data from the web. "+ANSI_RESET);
         boolean crawlForData;
         Scanner in = new Scanner(System.in);
         int choice  = in.nextInt();
@@ -55,40 +61,48 @@ public class StoiximanApp {
             System.out.println(ANSI_GREEN + "\nCRAWLING STARTED" + ANSI_RESET);
 
             // URL of index page
-            String indexPageUrl = stoiximan.getIndexPageUrl();
+            String indexPageUrl = stoiximanParser.getIndexPageUrl();
             // connect and fetch the index page
-            Document indexDocument = stoiximan.connectAndFetchPage(indexPageUrl);
+            Document indexDocument = stoiximanParser.connectAndFetchPage(indexPageUrl);
 
-            // create the sport links list
+            // get bettor data
             try {
-                stoiximan.fetchSportLinks(indexDocument);
+                stoiximanParser.fetchBettorData(indexDocument);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // get sport data and create the sport links list
+            try {
+                stoiximanParser.fetchSportData(indexDocument);
             } catch (Exception e) {
                 e.printStackTrace();
             }
             // URLs of sport links
-            List<String> sportLinks = stoiximan.getSportLinks();
+            List<String> sportLinks = stoiximanParser.getSportLinks();
 
             // visit every sport link
             for (String sportLink : sportLinks) {
 
-                Document sportDocument = stoiximan.connectAndFetchPage(sportLink);
+                Document sportDocument = stoiximanParser.connectAndFetchPage(sportLink);
 
                 // create the event links list
                 try {
-                    stoiximan.fetchEventLinks(sportDocument);
+                    stoiximanParser.fetchEventData(sportDocument);
                 } catch (Exception e) {
                     pagesNotLoaded.add(sportLink);
                 }
-                // URLs of event links
-                List<String> eventLinks = stoiximan.getEventLinks();
 
-                String sportToCrawl = stoiximan.getSport();
-                System.out.println(ANSI_PURPLE + "\nExtracting data for: " + sportToCrawl + ANSI_RESET);
-                System.out.println(ANSI_PURPLE + "----------------------------------------" + ANSI_RESET);
+                // URLs of event links
+                List<String> eventLinks = stoiximanParser.getEventLinks();
+
+                // String sportToCrawl = stoiximanParser.getSportName();
+                // System.out.println(ANSI_PURPLE + "\nExtracting data for: " + sportToCrawl + ANSI_RESET);
+                // System.out.println(ANSI_PURPLE + "----------------------------------------" + ANSI_RESET);
 
                 // progress bar
                 int max = eventLinks.size();
-                try (ProgressBar bar = new ProgressBar(sportToCrawl + " progress", max)) {
+                try (ProgressBar bar = new ProgressBar("Progress", max)) {
 
                     // visit every event
                     for (String eventLink : eventLinks) {
@@ -97,34 +111,33 @@ public class StoiximanApp {
 
                         // System.out.println("-----------> Event link: " + eventLink);
 
-                        Document eventDocument = stoiximan.connectAndFetchPage(eventLink);
+                        Document eventDocument = stoiximanParser.connectAndFetchPage(eventLink);
 
                         // create the game links list
                         try {
-                            stoiximan.fetchGameLinks(eventDocument);
+                            stoiximanParser.fetchGameData(eventDocument);
                         } catch (Exception e) {
                             pagesNotLoaded.add(eventLink);
                         }
 
                         // URLs of game links
-                        List<String> gameLinks = stoiximan.getMatchLinks();
+                        List<String> gameLinks = stoiximanParser.getGameLinks();
 
                         // visit every match
                         for (String gameLink : gameLinks) {
 
                             // System.out.println("-----------> Game link: "+gameLink);
 
-                            Document gameDocument = stoiximan.connectAndFetchPage(gameLink);
+                            Document gameDocument = stoiximanParser.connectAndFetchPage(gameLink);
 
                             // fetch final data (data for match and bets)
                             try {
-                                stoiximan.fetchFinalData(gameDocument);
+                                stoiximanParser.fetchBetData(gameDocument);
                             } catch (Exception e) {
                                 pagesNotLoaded.add(gameLink);
+                                System.out.println(e.getMessage());
                             }
 
-                            // save data to database
-                            stoiximan.saveFinalData();
                         }
                     }
                 }
@@ -132,15 +145,31 @@ public class StoiximanApp {
 
             // if any page did not loaded correctly
             if (pagesNotLoaded.size() > 0) {
+                System.out.println("");
                 for (String page : pagesNotLoaded) {
-                    System.out.println(ANSI_YELLOW + "\nPage: " + page + " did not loaded correctly" + ANSI_RESET);
+                    System.out.println(ANSI_YELLOW + "Page: " + page + " did not loaded correctly" + ANSI_RESET);
                 }
             } else {
                 System.out.println(ANSI_CYAN + "\nAll pages has been loaded correctly" + ANSI_RESET);
             }
 
             System.out.println("\nYou can now access your database to check the results!");
-            System.out.println(ANSI_GREEN + "\nCRAWLING ENDED" + ANSI_RESET);
+            System.out.println(ANSI_GREEN + "\nCRAWLING ENDED\n" + ANSI_RESET);
+
+            // program finished
+            double endTime = System.nanoTime();
+            //Time in nanoseconds
+            double totalTime = endTime - startTime;
+            //Time in seconds
+            double totalTimeInSec = totalTime / 1000000000.0;
+
+            System.out.println(ANSI_PURPLE + "Total time running: " + ANSI_RESET + totalTimeInSec + " seconds");
+            System.out.println(ANSI_PURPLE + "Total time running: " + ANSI_RESET + totalTimeInSec / 60 + " minutes");
         }
+    }
+
+    @Bean
+    public SpringDataDialect springDataDialect() {
+        return new SpringDataDialect();
     }
 }
