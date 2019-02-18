da0c9105db7a8b6ca466e22abcb6e4b97c8e565b
kavou <kavourdiris@windowslive.com>
2019-02-18 17:12:01 +0200

    Main class will be used later

--- src/main/java/com/kavou/bettingCrawler/BettingCrawlerApplication.java
+++ src/main/java/com/kavou/bettingCrawler/BettingCrawlerApplication.java
@@ -1,6 +1,5 @@
 package com.kavou.bettingCrawler;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
